05debd13d9a3c13a2a96d02309225946f71cf30b
georgejr <methungeorge333@gmail.com>
2024-07-02 17:03:01 +0200

    add GitHub action

--- tests/testthat/test_openASD.R
+++ tests/testthat/test_openASD.R
@@ -1,9 +1,19 @@
 context("opening spectra files")
 
-test_that("open ASD spectra file", {
+test_that("open single ASD spectra file", {
   
   file.dir <- system.file("extdata/PM01_TIAM_B_LC_REFL00005.asd",package="FieldSpectra")
   spec <- read.asd(file.dir, out.dir=file.path('~'), start.wave=350, end.wave=2500, step.size=1)
   expect_equal(round(sum(spec$Spectra)), 517)
   
 })
+
+# test_that("open multiple ASD spectra file", {
+#   
+#   file.dir <- system.file("extdata/",package="FieldSpectra")
+#   spec <- read.asd(file.dir, out.dir=file.path('~'), start.wave=350, end.wave=2500, step.size=1)
+#   expect_equal(round(sum(spec$Spectra)), 517)
+#   
+# })
+# 
+
