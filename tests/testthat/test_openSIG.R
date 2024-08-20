60c8e49e742fb3f8f77c08daf9edebf23b18625e
georgejr <methungeorge333@gmail.com>
2024-08-20 10:45:56 +0200

    Fix SVC unit test

--- tests/testthat/test_openSIG.R
+++ tests/testthat/test_openSIG.R
@@ -1,7 +1,3 @@
-# Load the testthat package
-library(testthat)
-source("/Users/methungeorge/Desktop/nearspectRa/R/read.sig.R")
-
 # Create test
 test_that("read.sig function works correctly with default parameter", {
   # Create a temporary directory and file for testing
@@ -18,13 +14,15 @@ test_that("read.sig function works correctly with default parameter", {
     "700 1 2 40"
   ), temp_file)
 
+  filepath <- system.file("extdata/gr070214_003.sig",package="FieldSpectra")
+  
   # Test with default = TRUE (all columns)
-  data_all <- read.sig(temp_file, default = TRUE)
+  data_all <- read.sig(filepath, default = TRUE)
   expect_equal(ncol(data_all), 5)  # Should have 5 columns
   expect_equal(colnames(data_all), c("Wavelengths(nm)", "Reference Values", "Target Values", "Reflectance(%)", "file_name"))
 
   # Test with default = FALSE (selected columns)
-  data_selected <- read.sig(temp_file, default = FALSE)
+  data_selected <- read.sig(filepath, default = FALSE)
   expect_equal(ncol(data_selected), 3)  # Should have 3 columns: Wavelengths(nm), Reflectance(%), file_name
   expect_equal(colnames(data_selected), c("Wavelengths(nm)", "Reflectance(%)", "file_name"))
 
